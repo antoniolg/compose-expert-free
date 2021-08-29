de9fa542d8518ee57ce078608e0c00a66ee888ef
Antonio Leiva <antonioleivag@gmail.com>
2021-08-30 00:27:35 +0200

    Play with Layouts and create MediaItem first iteration

--- app/src/main/java/com/antonioleiva/mymovies/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/MainActivity.kt
@@ -3,11 +3,17 @@ package com.antonioleiva.mymovies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.antonioleiva.mymovies.ui.theme.MyMoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,16 +30,51 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MediaItem() {
+    Column {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(color = Color.Red)
+        ) {
+
+        }
+        Text("Title 1")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier,
+        text = "Hello $name!"
+    )
 }
 
 @Preview(showBackground = true, name = "Android Greeting")
 @Composable
 fun DefaultPreview() {
     MyMoviesTheme {
-        Greeting("Antonio")
-        Greeting("Android")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Greeting(
+                name = "Android",
+                modifier = Modifier
+                    .background(Color.LightGray)
+            )
+            Greeting(
+                name = "Antonio",
+                modifier = Modifier
+                    .background(Color.Yellow)
+            )
+        }
     }
 }
