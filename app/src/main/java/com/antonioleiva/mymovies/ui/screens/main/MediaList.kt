ricardomorarey/compose

Reorganize code

app/src/main/java/com/antonioleiva/mymovies/ui/screens/main/MediaList.kt
@@ -1,0 +1,112 @@
+package com.antonioleiva.mymovies.ui.screens.main
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayCircleOutline
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.antonioleiva.mymovies.R
+import com.antonioleiva.mymovies.model.MediaItem
+import com.antonioleiva.mymovies.model.getMedia
+import com.antonioleiva.mymovies.ui.MyMoviesApp
+
+@ExperimentalFoundationApi
+@ExperimentalCoilApi
+@Preview
+@Composable
+fun MediaList(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(dimensionResource(R.dimen.cell_min_width)),
+        contentPadding = PaddingValues(dimensionResource(R.dimen.padding_xsmall)),
+        modifier = modifier
+    ) {
+        items(getMedia()) {
+            MediaListItem(
+                mediaItem = it,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_xsmall))
+            )
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun MediaListItem(mediaItem: MediaItem, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        Thumb(mediaItem)
+        Title(mediaItem)
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+private fun Thumb(mediaItem: MediaItem) {
+    Box(
+        modifier = Modifier
+            .height(dimensionResource(R.dimen.cell_thumb_height))
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = rememberImagePainter(
+                data = mediaItem.thumb
+            ),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        if (mediaItem.type == MediaItem.Type.VIDEO) {
+            Icon(
+                Icons.Default.PlayCircleOutline,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(dimensionResource(R.dimen.cell_play_icon_size))
+                    .align(Alignment.Center),
+                tint = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+private fun Title(mediaItem: MediaItem) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.secondary)
+            .padding(dimensionResource(R.dimen.padding_medium))
+    ) {
+        Text(
+            text = mediaItem.title,
+            style = MaterialTheme.typography.h6
+        )
+    }
+}
+
+@ExperimentalCoilApi
+@Preview
+@Composable
+fun MediaListItemPreview() {
+    MyMoviesApp {
+        val mediaItem = MediaItem(1, "Item 1", "", MediaItem.Type.VIDEO)
+        MediaListItem(mediaItem = mediaItem)
+    }
+}

antoniolg/compose-expert-free

app/src/main/java/com/antonioleiva/mymovies/ui/screens/main/MediaList.kt
@@ -1,0 +1,104 @@
+package com.antonioleiva.mymovies.ui.screens.main
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayCircleOutline
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import com.antonioleiva.mymovies.R
+import com.antonioleiva.mymovies.model.MediaItem
+import com.antonioleiva.mymovies.model.getMedia
+import com.antonioleiva.mymovies.ui.MyMoviesApp
+
+@ExperimentalFoundationApi
+@Preview
+@Composable
+fun MediaList(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(dimensionResource(R.dimen.cell_min_width)),
+        contentPadding = PaddingValues(dimensionResource(R.dimen.padding_xsmall)),
+        modifier = modifier
+    ) {
+        items(getMedia()) {
+            MediaListItem(
+                mediaItem = it,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_xsmall))
+            )
+        }
+    }
+}
+
+@Composable
+fun MediaListItem(mediaItem: MediaItem, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        Thumb(mediaItem)
+        Title(mediaItem)
+    }
+}
+
+@Composable
+private fun Thumb(mediaItem: MediaItem) {
+    Box(
+        modifier = Modifier
+            .height(dimensionResource(R.dimen.cell_thumb_height))
+            .fillMaxWidth()
+    ) {
+        AsyncImage(
+            model = mediaItem.thumb,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        if (mediaItem.type == MediaItem.Type.VIDEO) {
+            Icon(
+                Icons.Default.PlayCircleOutline,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(dimensionResource(R.dimen.cell_play_icon_size))
+                    .align(Alignment.Center),
+                tint = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+private fun Title(mediaItem: MediaItem) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.secondary)
+            .padding(dimensionResource(R.dimen.padding_medium))
+    ) {
+        Text(
+            text = mediaItem.title,
+            style = MaterialTheme.typography.h6
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MediaListItemPreview() {
+    MyMoviesApp {
+        val mediaItem = MediaItem(1, "Item 1", "", MediaItem.Type.VIDEO)
+        MediaListItem(mediaItem = mediaItem)
+    }
+}